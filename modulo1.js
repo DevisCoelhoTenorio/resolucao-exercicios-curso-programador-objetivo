b89b11fea8479b05259bfaf64a0afb347b9bfbf5
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-11-22 15:06:03 -0300

    feat: requisitos 01, 02 e 03 resolvidos

---
+++ modulo1.js
@@ -0,0 +1,22 @@
+// 1. Crie uma função que declara uma variável sem atribuir valor a ela
+// e retorna essa variável;
+
+function declareVar() {
+    var a;
+    return a
+};
+
+// 2. Crie uma função que declara uma variável, atribuindo valor a ela
+// e retorna essa variável;
+
+function declareVarValue(a) {
+    var varValue = a;
+    return varValue
+};
+
+// 3. Crie uma função que retorne a soma de três números quaisquer.
+
+function sum(a, b, c) {
+    return a + b + c
+};
+
