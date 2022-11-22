8e6eae34582f623bb197a6ea75ccc665b078ff91
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-11-22 15:15:04 -0300

    feat: requisitos 4 e 5 resolvidos

--- modulo1.js
+++ modulo1.js
@@ -3,7 +3,7 @@
 
 function declareVar() {
     var a;
-    return a
+    return a;
 };
 
 // 2. Crie uma função que declara uma variável, atribuindo valor a ela
@@ -11,12 +11,26 @@ function declareVar() {
 
 function declareVarValue(a) {
     var varValue = a;
-    return varValue
+    return varValue;
 };
 
 // 3. Crie uma função que retorne a soma de três números quaisquer.
 
 function sum(a, b, c) {
-    return a + b + c
+    return a + b + c;
 };
 
+// 4. Crie uma função que verifica se um numero é maior que outro, usando o
+// if e o else. ex: ehMaior(2,1) => true; ehMaior(1,2) => false
+
+function ehMaior(a, b) {
+    if (a > b) return true;
+    else return false;
+};
+
+// 5. Crie uma função que verifica se um numero é maior que outro, SEM usar o
+// if e o else. ex: ehMaior(2,1) => true; ehMaior(1,2) => false
+
+function ehMaiorNoIf(a, b) {
+    return a > b ? true : false;
+};
