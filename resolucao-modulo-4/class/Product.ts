9298b368226ac56158736f69adf8c385ca107e46
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-11-23 13:11:01 -0300

    fix: Organizando pastas e adicionando instruções

--- resolucao-modulo-4/class/Product.ts
+++ resolucao-modulo-4/class/Product.ts
@@ -1,26 +1,32 @@
-import { userProducts } from './dataBase';
-import IProduct from './interfaces/IProduct';
+import { userProducts } from '../data/dataBase';
+import IProduct from '../interfaces/IProduct';
+
+// Obs: Para rodar os Logs de teste basta digitar o comando
+// ts-node ./resolucao-modulo-4/class/Product.ts na raiz do projeto.
 
 export default class Product {
   private _productList: IProduct[];
 
   constructor(){
-    this._productList = userProducts
+    this._productList = userProducts;
   };
+
   private findUserProducts(prop: string): IProduct[] {
     return this._productList.filter((product) => product[prop] === prop);
   };
+
   // 10. Verificar se um dado produto foi consumido
   // mais de uma vez.
   public consumedProduct(productName: string): boolean {
-    const findListProduct = this.findUserProducts(productName)
-    return findListProduct.length > 1
+    const findListProduct = this.findUserProducts(productName);
+    return findListProduct.length > 1;
   };
+
   // 11. Verificar se um dado produto foi consumido
   // mais de um usuário.
   public consumedByDifferentUser(productName: string): boolean {
-    const findListProduct = this.findUserProducts(productName)
-    const productMap: Record<string, number> = {}
+    const findListProduct = this.findUserProducts(productName);
+    const productMap: Record<string, number> = {};
     let check = false;
     findListProduct.forEach((product) => {
       if (productMap[product.userId]) check = true;
@@ -28,6 +34,7 @@ export default class Product {
     });
     return check;
   };
+
   // 12. Verificar se existe algum produto
   // que foi comprado por mais de um usuário
   public boughtByDifferentUser(): boolean {
@@ -39,6 +46,7 @@ export default class Product {
     });
     return check;
   };
+
   // Método auxiliar para Req 19 e 20
   public getDifferentProduct(): Record<string, IProduct> {
     const differentProductMap: Record<string, IProduct> = {};
@@ -47,17 +55,21 @@ export default class Product {
     });
     return differentProductMap;
   };
+
   // 19. Obter os nomes distintos de produtos;
   public getDifferentNameProduct(): Array<string> {
     const differentNames = Object.keys(this.getDifferentProduct());
     return differentNames;
   };
+
   // Método auxiliar do Req 20;
   public averagePriceProducts(): number {
     const differentProducts = Object.values(this.getDifferentProduct());
-    const averagePrice = (differentProducts.reduce((acc, crr) => acc += crr.price ,0)) / differentProducts.length;
+    const averagePrice = (differentProducts
+      .reduce((acc, crr) => acc += crr.price ,0)) / differentProducts.length;
     return averagePrice;
   };
+
   public listUserMapBySpending(): Record<string, number> {
     const userMapBySpending: Record<string, number>= {};
     this._productList.forEach((product) => {
@@ -66,6 +78,7 @@ export default class Product {
     });
     return userMapBySpending;
   };
+  
   // Método auxiliar do Req 22;
   public listUserMapByAmountPurchases() {
     const userMapByAmountPurchases: Record<string, number>= {};
--- resolucao-modulo-4/class/User.ts
+++ resolucao-modulo-4/class/User.ts
@@ -1,9 +1,10 @@
-import { users, newUsers } from './dataBase';
-import IUser from './interfaces/IUser';
+import { users, newUsers } from '../data/dataBase';
+import IUser from '../interfaces/IUser';
 import ProductService from './Product';
-// import IProduct from './interfaces/IProduct';
-import IImc from './interfaces/IImc';
+import IImc from '../interfaces/IImc';
 
+// Obs: Para rodar os Logs de teste basta digitar o comando
+// ts-node ./resolucao-modulo-4/class/User.ts na raiz do projeto.
 
 const USER_NOT_FOUND = 'User not found';
 export default class User {
@@ -35,24 +36,32 @@ export default class User {
     })
     return list[0];
   };
+
   // 4. Encontre os usuários de um dado estado e com peso maior
   // que um dado peso;
   public findByMaxWeightAndCity(weight: number, city: string): IUser[] {
     const userMap = this._userList.filter((user)=> user.city === city && user.weight > weight);
     return userMap;
   };
+
   // 5. Encontre os usuários de um dado estado ou que são casados
   public findByStateOrMarried(city: string): IUser[] {
     const userMap = this._userList.filter((user) => user.city === city || user.married);
     return userMap;
   };
+
   // 6. Cria uma função que recebe a lista de usuários e retorna
   // uma lista com o IMC dos respectivos usuários;
   // OBS: imc = peso/(altura x altura)
   public listUserByImc(): IImc[] {
-    const userMap = this._userList.map((user) => ({[user.name]: (user.weight/(user.height * user.height)).toFixed(2)}));
+    const userMap = this._userList.map((user) => (
+      {
+      [user.name]: (user.weight/(user.height * user.height)).toFixed(2),
+      }
+    ));
     return userMap;
   };
+
   // 7. Cria uma função que recebe a lista de usuários e retorna
   // a lista dos nomes completos capitalizados;
   // OBS: o nome 'joao da silva' capitalizado, seria: Joao Da Silva
@@ -65,42 +74,50 @@ export default class User {
     const text = word[0].toUpperCase() + word.slice(1);
     return text;
   };
+
   private capitalizeText(text: string): string {
     const arrayWord = text.split(' ');
     const arrayMap = arrayWord.map((word) => this.capitalizeWord(word));
     return arrayMap.join(' ');
   };
+
   public listName(): string[] {
-    return this._userList.map((user) => this.capitalizeText(user.name))
+    return this._userList.map((user) => this.capitalizeText(user.name));
   };
+
   // 8. Calcular a média de altura de todos os usuários;
   public averageHeight(): string {
-    return ((this._userList.reduce((acc, crr) => acc += crr.height ,0)) / this._userList.length).toFixed(2)
+    return ((this._userList.reduce((acc, crr) => acc += crr.height ,0)) / this._userList.length).toFixed(2);
   };
+
   // 9. Retornar os usuários com altura abaixo da média;
   public listBelowAverageHeight(): IUser[] {
     const avarageHeight = Number(this.averageHeight());
     return this._userList.filter((user) => user.height < avarageHeight);
   }; 
+
   // 13. Retornar a lista de usuários sem o usuário mais novo da lista.
   public excludeYoungestFromList(): IUser[] {
     const list = [...this._userList];
     list.sort((a, b) => b.age - a.age).pop();
     return list;
   };
+
   // 14. Retornar um objeto que mapeia usuários pelo próprio
   // id do usuário.
   public listUserMapForId(): Record<string, IUser> {
     const userMap: Record<string, IUser> = {};
-    this._userList.forEach((user) => userMap[user.id] = user)
+    this._userList.forEach((user) => userMap[user.id] = user);
     return userMap;
   };
+
   // 15. Contar a quantidade de usuários de uma dada cidade;
   public countUserBySpecificCity(city: string): number {
     let count: number = 0;
-    this._userList.forEach((user) => user.city === city ? count += 1 : null)
+    this._userList.forEach((user) => user.city === city ? count += 1 : null);
     return count;
   };
+
   // 16. Agrupar usuários pela cidade;
   public groupByCity(): Record<string, IUser[]> {
     const cityMap: Record<string, IUser[]> = {};
@@ -110,18 +127,20 @@ export default class User {
     });
     return cityMap;
   };
+
   // 17. Contar a quantidade de usuários por cidade;
   public countUserByCity(): Record<string, number> {
-    const cityCountMap: Record<string, number> = {}
+    const cityCountMap: Record<string, number> = {};
     this._userList.forEach((user) => {
       if (cityCountMap[user.city]) cityCountMap[user.city] += 1;
       else cityCountMap[user.city] = 1;
     });
     return cityCountMap;
   };
+
   // 18. Obter a média salarial dos usuários por cidade;**
   public  averageSalaryByCity(): Record<string, string> {
-    const averageSalaryMap: Record<string, {averageSalary: number, countUser: number}> = {}
+    const averageSalaryMap: Record<string, {averageSalary: number, countUser: number}> = {};
     this._userList.forEach((user) => {
       if (averageSalaryMap[user.city]) {
         averageSalaryMap[user.city].averageSalary += user.salary;
@@ -129,15 +148,18 @@ export default class User {
       }
       else averageSalaryMap[user.city] = {
        averageSalary: user.salary,
-       countUser: 1 
+       countUser: 1,
       };
     });
     const responseObj: Record<string, string> = {}
     Object.keys(averageSalaryMap).forEach((city) => {
-      responseObj[city] = (averageSalaryMap[city].averageSalary / averageSalaryMap[city].countUser).toFixed(2);
+      responseObj[city] = (
+        averageSalaryMap[city].averageSalary / averageSalaryMap[city].countUser)
+        .toFixed(2);
     });
     return responseObj;
   };
+
   // 20. Retornar os usuários que gastaram mais que preço
   // médio dos produtos vendidos;
   public listUserByAboveAverage(): IUser[] {
@@ -145,16 +167,20 @@ export default class User {
     const listUserBySpending = this._productService.listUserMapBySpending();
     const selectedUSers: IUser[] = [];
     this._userList.forEach((user) => {
-      if (listUserBySpending[user.id] && listUserBySpending[user.id] > averagePrice) selectedUSers.push(user);
+      if (listUserBySpending[user.id] && listUserBySpending[user.id] > averagePrice) {
+        selectedUSers.push(user);
+      }
     });
     return selectedUSers;
   };
+
   // 21. Encontre o userId que menos gastou;
   public findLeastSpentUser(): string {
     const listUserBySpending = Object.entries(this._productService.listUserMapBySpending());
     listUserBySpending.sort((a, b) => a[1] - b[1]);
     return listUserBySpending[0][0];
   };
+
   // 22. Encontre o userId que comprou menos produtos, mas
   // que comprou sim algum produto;
   public findUserIdByBoughtLess(): string {
@@ -162,6 +188,7 @@ export default class User {
     listUserByBoughtLess.sort((a, b) => a[1] - b[1]);
     return listUserByBoughtLess[0][0];
   };
+
   // 23. Encontre os usuários (objetos completos)
   // que compraram algum produto;
   public getUsersByBought(): IUser[] {
@@ -172,6 +199,7 @@ export default class User {
     });
     return userBoughtMap;
   };
+
   // 24. Encontre os usuários comuns a duas listas de usuários.
   // Dois usuários são idênticos, se tiverem o mesmo id;
   // ex: lista1=[{id:2}, {id:3}] e lista2=[{id:3},{id:4}]
@@ -186,6 +214,7 @@ export default class User {
     });
     return Object.values(filterList);
   };
+
   // 25. Encontre os usuários não-comuns a duas listas de usuários.
   // ex: lista1=[{id:2}, {id:3}] e lista2=[{id:3},{id:4}]
   // usuariosNaoComuns => [{id:2}, {id:4}]
@@ -199,8 +228,10 @@ export default class User {
     return Object.values(filterList);
   };
 };
+
 const productService = new ProductService();
 const service = new User(productService);
+
 // Req 01
 console.log('Req 01', service.findByName('josé da silva'));
 // Req 02
@@ -243,5 +274,3 @@ console.log('Req 23', service.getUsersByBought());
 console.log('Req 24', service.findSimilarUsers(newUsers));
 // Req 25
 console.log('Req 25', service.findNoSimilarUsers(newUsers));
-
-
