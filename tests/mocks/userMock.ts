eb70c8a8b1eae8aba23a7161c543ff2cd3b7bb77
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-11-23 22:41:11 -0300

    fix: Arrumando arquivos de Mock

--- tests/mocks/userMock.ts
+++ tests/mocks/userMock.ts
@@ -50,3 +50,499 @@ export const expectedReq03 = {
       active: true,
   },
 };
+
+export const expectedReq04 = [
+  {
+    id: 6,
+    city: "São Paulo",
+    name: "zé rico programador",
+    age: 62,
+    weight: 90,
+    sex: "m",
+    height: 1.82,
+    salary: 30000, // 30 mil
+    married: false,
+    active: true,
+  },
+];
+
+export const expectedReq05 = [
+  {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+  {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+  {
+    id: 4,
+    city: 'Rio de Janeiro',
+    name: 'silva melo de aguiar',
+    age: 38,
+    weight: 70,
+    sex: 'm',
+    height: 1.85,
+    salary: 5000,
+    married: true,
+    active: true
+  },
+  {
+    id: 5,
+    city: 'São Paulo',
+    name: 'joão carvalho da conceição',
+    age: 48,
+    weight: 70,
+    sex: 'm',
+    height: 1.9,
+    salary: 9000,
+    married: false,
+    active: true
+  },
+  {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  }
+];
+
+export const expectedReq06 = [
+  { 'josé da silva': '24.22' },
+  { 'maria clementina correia': '27.34' },
+  { 'antonieta francesa rainha': '25.71' },
+  { 'silva melo de aguiar': '20.45' },
+  { 'joão carvalho da conceição': '19.39' },
+  { 'zé rico programador': '27.17' }
+];
+
+export const expectedReq07 = [
+  'José Da Silva',
+  'Maria Clementina Correia',
+  'Antonieta Francesa Rainha',
+  'Silva Melo De Aguiar',
+  'João Carvalho Da Conceição',
+  'Zé Rico Programador'
+];
+
+export const expectedReq09 = [
+  {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+  {
+    id: 2,
+    city: 'Rio de Janeiro',
+    name: 'maria clementina correia',
+    age: 17,
+    weight: 70,
+    sex: 'f',
+    height: 1.6,
+    salary: 2000,
+    married: false,
+    active: true
+  },
+  {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+];
+
+export const expectedReq13 = [
+  {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  },
+  {
+    id: 5,
+    city: 'São Paulo',
+    name: 'joão carvalho da conceição',
+    age: 48,
+    weight: 70,
+    sex: 'm',
+    height: 1.9,
+    salary: 9000,
+    married: false,
+    active: true
+  },
+  {
+    id: 4,
+    city: 'Rio de Janeiro',
+    name: 'silva melo de aguiar',
+    age: 38,
+    weight: 70,
+    sex: 'm',
+    height: 1.85,
+    salary: 5000,
+    married: true,
+    active: true
+  },
+  {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+  {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+];
+
+export const expectedReq14 = {
+  '1': {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+  '2': {
+    id: 2,
+    city: 'Rio de Janeiro',
+    name: 'maria clementina correia',
+    age: 17,
+    weight: 70,
+    sex: 'f',
+    height: 1.6,
+    salary: 2000,
+    married: false,
+    active: true
+  },
+  '3': {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+  '4': {
+    id: 4,
+    city: 'Rio de Janeiro',
+    name: 'silva melo de aguiar',
+    age: 38,
+    weight: 70,
+    sex: 'm',
+    height: 1.85,
+    salary: 5000,
+    married: true,
+    active: true
+  },
+  '5': {
+    id: 5,
+    city: 'São Paulo',
+    name: 'joão carvalho da conceição',
+    age: 48,
+    weight: 70,
+    sex: 'm',
+    height: 1.9,
+    salary: 9000,
+    married: false,
+    active: true
+  },
+  '6': {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  }
+};
+
+export const expectedReq16 = {
+  'São Paulo': [
+    {
+      id: 1,
+      city: 'São Paulo',
+      name: 'josé da silva',
+      age: 21,
+      weight: 70,
+      sex: 'm',
+      height: 1.7,
+      salary: 1000,
+      married: false,
+      active: true
+    },
+    {
+      id: 5,
+      city: 'São Paulo',
+      name: 'joão carvalho da conceição',
+      age: 48,
+      weight: 70,
+      sex: 'm',
+      height: 1.9,
+      salary: 9000,
+      married: false,
+      active: true
+    },
+    {
+      id: 6,
+      city: 'São Paulo',
+      name: 'zé rico programador',
+      age: 62,
+      weight: 90,
+      sex: 'm',
+      height: 1.82,
+      salary: 30000,
+      married: false,
+      active: true
+    }
+  ],
+  'Rio de Janeiro': [
+    {
+      id: 2,
+      city: 'Rio de Janeiro',
+      name: 'maria clementina correia',
+      age: 17,
+      weight: 70,
+      sex: 'f',
+      height: 1.6,
+      salary: 2000,
+      married: false,
+      active: true
+    },
+    {
+      id: 3,
+      city: 'Rio de Janeiro',
+      name: 'antonieta francesa rainha',
+      age: 28,
+      weight: 70,
+      sex: 'f',
+      height: 1.65,
+      salary: 3000,
+      married: true,
+      active: true
+    },
+    {
+      id: 4,
+      city: 'Rio de Janeiro',
+      name: 'silva melo de aguiar',
+      age: 38,
+      weight: 70,
+      sex: 'm',
+      height: 1.85,
+      salary: 5000,
+      married: true,
+      active: true
+    }
+  ]
+};
+
+export const expectedReq20 = [
+  {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  },
+];
+
+export const expectedReq23 = [
+  {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+  {
+    id: 2,
+    city: 'Rio de Janeiro',
+    name: 'maria clementina correia',
+    age: 17,
+    weight: 70,
+    sex: 'f',
+    height: 1.6,
+    salary: 2000,
+    married: false,
+    active: true
+  },
+  {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+  {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  }
+];
+
+export const expectedReq24 = [
+  {
+    id: 1,
+    city: 'São Paulo',
+    name: 'josé da silva',
+    age: 21,
+    weight: 70,
+    sex: 'm',
+    height: 1.7,
+    salary: 1000,
+    married: false,
+    active: true
+  },
+  {
+    id: 2,
+    city: 'Rio de Janeiro',
+    name: 'maria clementina correia',
+    age: 17,
+    weight: 70,
+    sex: 'f',
+    height: 1.6,
+    salary: 2000,
+    married: false,
+    active: true
+  },
+  {
+    id: 3,
+    city: 'Rio de Janeiro',
+    name: 'antonieta francesa rainha',
+    age: 28,
+    weight: 70,
+    sex: 'f',
+    height: 1.65,
+    salary: 3000,
+    married: true,
+    active: true
+  },
+  {
+    id: 4,
+    city: 'Rio de Janeiro',
+    name: 'silva melo de aguiar',
+    age: 38,
+    weight: 70,
+    sex: 'm',
+    height: 1.85,
+    salary: 5000,
+    married: true,
+    active: true
+  }
+];
+
+export const expectedReq25 = [
+  {
+    id: 5,
+    city: 'São Paulo',
+    name: 'joão carvalho da conceição',
+    age: 48,
+    weight: 70,
+    sex: 'm',
+    height: 1.9,
+    salary: 9000,
+    married: false,
+    active: true
+  },
+  {
+    id: 6,
+    city: 'São Paulo',
+    name: 'zé rico programador',
+    age: 62,
+    weight: 90,
+    sex: 'm',
+    height: 1.82,
+    salary: 30000,
+    married: false,
+    active: true
+  }
+];
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -2,7 +2,23 @@ import { expect } from 'chai';
 import User from '../resolucao-modulo-4/class/User';
 import ProductService from '../resolucao-modulo-4/class/Product';
 import { newUsers } from '../resolucao-modulo-4/data/dataBase';
-import { expectedReq01, expectedReq02, expectedReq03 } from './mocks/userMock';
+import {
+  expectedReq01,
+  expectedReq02,
+  expectedReq03,
+  expectedReq04,
+  expectedReq05,
+  expectedReq06,
+  expectedReq07,
+  expectedReq09,
+  expectedReq13,
+  expectedReq14,
+  expectedReq16,
+  expectedReq20,
+  expectedReq23,
+  expectedReq24,
+  expectedReq25
+} from './mocks/userMock';
 
 describe('Testa a class User', function () {
     const productService = new ProductService();
@@ -28,115 +44,22 @@ describe('Testa a class User', function () {
     });
 
     it ('04- Verifica se é possível encontre os usuários de um dado estado e com peso maior que um dado peso', () => {
-      const expected = [
-        {
-          id: 6,
-          city: "São Paulo",
-          name: "zé rico programador",
-          age: 62,
-          weight: 90,
-          sex: "m",
-          height: 1.82,
-          salary: 30000, // 30 mil
-          married: false,
-          active: true,
-        },
-      ];
       const result = service.findByMaxWeightAndCity(75, 'São Paulo');
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq04);
     });
     it ('05- Verifica se é possível encontre os usuários de um dado estado ou que são casados', () => {
-      const expected = [
-        {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-        {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-        {
-          id: 4,
-          city: 'Rio de Janeiro',
-          name: 'silva melo de aguiar',
-          age: 38,
-          weight: 70,
-          sex: 'm',
-          height: 1.85,
-          salary: 5000,
-          married: true,
-          active: true
-        },
-        {
-          id: 5,
-          city: 'São Paulo',
-          name: 'joão carvalho da conceição',
-          age: 48,
-          weight: 70,
-          sex: 'm',
-          height: 1.9,
-          salary: 9000,
-          married: false,
-          active: true
-        },
-        {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        }
-      ];
-
       const result = service.findByStateOrMarried('São Paulo');
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq05);
     });
 
-    it ('06- Verifica se é possível obter uma lista de usuários com os IMC', () => {
-      const expected =  [
-        { 'josé da silva': '24.22' },
-        { 'maria clementina correia': '27.34' },
-        { 'antonieta francesa rainha': '25.71' },
-        { 'silva melo de aguiar': '20.45' },
-        { 'joão carvalho da conceição': '19.39' },
-        { 'zé rico programador': '27.17' }
-      ];
+    it ('06- Verifica se é possível obter uma lista de usuários com os IMC', () => { 
       const result = service.listUserByImc();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq06);
     });
 
     it ('07- Verifica se é possível obter uma lista dos nomes completos capitalizados', () => {
-      const expected = [
-        'José Da Silva',
-        'Maria Clementina Correia',
-        'Antonieta Francesa Rainha',
-        'Silva Melo De Aguiar',
-        'João Carvalho Da Conceição',
-        'Zé Rico Programador'
-      ];
       const result = service.listName();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq07);
     });
 
     it ('08- Verifica se é possível calcular a média de altura de todos os usuários', () => {
@@ -146,192 +69,18 @@ describe('Testa a class User', function () {
     });
 
     it ('09- Verifica se é possível obter os usuários com altura abaixo da média', () => {
-      const expected = [
-        {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-        {
-          id: 2,
-          city: 'Rio de Janeiro',
-          name: 'maria clementina correia',
-          age: 17,
-          weight: 70,
-          sex: 'f',
-          height: 1.6,
-          salary: 2000,
-          married: false,
-          active: true
-        },
-        {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-      ];
       const result = service.listBelowAverageHeight();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq09);
     });
 
     it ('13- Verificar se possível obter uma lista de usuários sem o usuário mais novo', () => {
-      const expected = [
-        {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        },
-        {
-          id: 5,
-          city: 'São Paulo',
-          name: 'joão carvalho da conceição',
-          age: 48,
-          weight: 70,
-          sex: 'm',
-          height: 1.9,
-          salary: 9000,
-          married: false,
-          active: true
-        },
-        {
-          id: 4,
-          city: 'Rio de Janeiro',
-          name: 'silva melo de aguiar',
-          age: 38,
-          weight: 70,
-          sex: 'm',
-          height: 1.85,
-          salary: 5000,
-          married: true,
-          active: true
-        },
-        {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-        {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-      ];
       const result = service.excludeYoungestFromList();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq13);
     });
 
     it ('14- Verifica se retornar um objeto que mapeia usuários pelo própri id do usuário.', () => {
-      const expected = {
-        '1': {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-        '2': {
-          id: 2,
-          city: 'Rio de Janeiro',
-          name: 'maria clementina correia',
-          age: 17,
-          weight: 70,
-          sex: 'f',
-          height: 1.6,
-          salary: 2000,
-          married: false,
-          active: true
-        },
-        '3': {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-        '4': {
-          id: 4,
-          city: 'Rio de Janeiro',
-          name: 'silva melo de aguiar',
-          age: 38,
-          weight: 70,
-          sex: 'm',
-          height: 1.85,
-          salary: 5000,
-          married: true,
-          active: true
-        },
-        '5': {
-          id: 5,
-          city: 'São Paulo',
-          name: 'joão carvalho da conceição',
-          age: 48,
-          weight: 70,
-          sex: 'm',
-          height: 1.9,
-          salary: 9000,
-          married: false,
-          active: true
-        },
-        '6': {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        }
-      };
       const result = service.listUserMapForId();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq14);
     });
 
     it ('15- Verifica se retorna a quantidade de usuários de uma dada cidade', () => {
@@ -342,86 +91,8 @@ describe('Testa a class User', function () {
     });
 
     it ('16- Verifica se é possível agrupar usuários pela cidade', () => {
-      const expected = {
-        'São Paulo': [
-          {
-            id: 1,
-            city: 'São Paulo',
-            name: 'josé da silva',
-            age: 21,
-            weight: 70,
-            sex: 'm',
-            height: 1.7,
-            salary: 1000,
-            married: false,
-            active: true
-          },
-          {
-            id: 5,
-            city: 'São Paulo',
-            name: 'joão carvalho da conceição',
-            age: 48,
-            weight: 70,
-            sex: 'm',
-            height: 1.9,
-            salary: 9000,
-            married: false,
-            active: true
-          },
-          {
-            id: 6,
-            city: 'São Paulo',
-            name: 'zé rico programador',
-            age: 62,
-            weight: 90,
-            sex: 'm',
-            height: 1.82,
-            salary: 30000,
-            married: false,
-            active: true
-          }
-        ],
-        'Rio de Janeiro': [
-          {
-            id: 2,
-            city: 'Rio de Janeiro',
-            name: 'maria clementina correia',
-            age: 17,
-            weight: 70,
-            sex: 'f',
-            height: 1.6,
-            salary: 2000,
-            married: false,
-            active: true
-          },
-          {
-            id: 3,
-            city: 'Rio de Janeiro',
-            name: 'antonieta francesa rainha',
-            age: 28,
-            weight: 70,
-            sex: 'f',
-            height: 1.65,
-            salary: 3000,
-            married: true,
-            active: true
-          },
-          {
-            id: 4,
-            city: 'Rio de Janeiro',
-            name: 'silva melo de aguiar',
-            age: 38,
-            weight: 70,
-            sex: 'm',
-            height: 1.85,
-            salary: 5000,
-            married: true,
-            active: true
-          }
-        ]
-      };
       const result = service.groupByCity();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq16);
     });
 
     it ('17- Verifica se é possível obter a quantidade de usuários por cidade', () => {
@@ -437,22 +108,8 @@ describe('Testa a class User', function () {
     });
 
     it ('20- Verifica se é possível obter os usuários que gastaram mais que preço médio dos produtos vendidos', () => {
-      const expected = [
-        {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        },
-      ];
       const result = service.listUserByAboveAverage();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq20);
     });
 
     it ('21- Verifica se é possível obter o userId que menos gastou', () => {
@@ -470,145 +127,17 @@ describe('Testa a class User', function () {
     }); 
 
     it ('23- Verifica se é possível obter os usuários que compraram algum produto', () => {
-      const expected = [
-        {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-        {
-          id: 2,
-          city: 'Rio de Janeiro',
-          name: 'maria clementina correia',
-          age: 17,
-          weight: 70,
-          sex: 'f',
-          height: 1.6,
-          salary: 2000,
-          married: false,
-          active: true
-        },
-        {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-        {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        }
-      ];
-
       const result = service.getUsersByBought();
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq23);
     });
 
     it ('24- Verifica se é possível encontrar os usuários comuns com outra listas de usuários', () => {
-      const expected = [
-        {
-          id: 1,
-          city: 'São Paulo',
-          name: 'josé da silva',
-          age: 21,
-          weight: 70,
-          sex: 'm',
-          height: 1.7,
-          salary: 1000,
-          married: false,
-          active: true
-        },
-        {
-          id: 2,
-          city: 'Rio de Janeiro',
-          name: 'maria clementina correia',
-          age: 17,
-          weight: 70,
-          sex: 'f',
-          height: 1.6,
-          salary: 2000,
-          married: false,
-          active: true
-        },
-        {
-          id: 3,
-          city: 'Rio de Janeiro',
-          name: 'antonieta francesa rainha',
-          age: 28,
-          weight: 70,
-          sex: 'f',
-          height: 1.65,
-          salary: 3000,
-          married: true,
-          active: true
-        },
-        {
-          id: 4,
-          city: 'Rio de Janeiro',
-          name: 'silva melo de aguiar',
-          age: 38,
-          weight: 70,
-          sex: 'm',
-          height: 1.85,
-          salary: 5000,
-          married: true,
-          active: true
-        }
-      ];
       const result = service.findSimilarUsers(newUsers);
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq24);
     });
 
     it ('25- Verifica se é possível encontre os usuários não-comuns com outra listas de usuários', () => {
-      const expected = [
-        {
-          id: 5,
-          city: 'São Paulo',
-          name: 'joão carvalho da conceição',
-          age: 48,
-          weight: 70,
-          sex: 'm',
-          height: 1.9,
-          salary: 9000,
-          married: false,
-          active: true
-        },
-        {
-          id: 6,
-          city: 'São Paulo',
-          name: 'zé rico programador',
-          age: 62,
-          weight: 90,
-          sex: 'm',
-          height: 1.82,
-          salary: 30000,
-          married: false,
-          active: true
-        }
-      ];
-
       const result = service.findNoSimilarUsers(newUsers);
-      expect(result).to.be.deep.equal(expected);
+      expect(result).to.be.deep.equal(expectedReq25);
     });
 });
